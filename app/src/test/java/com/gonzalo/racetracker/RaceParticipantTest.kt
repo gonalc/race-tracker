5dc96a70e78d5b49675301b0824f0ae835bc790b
Gonzalo <gonalc8@gmail.com>
2025-02-01 11:58:12 +0100

    add test cases for the race participant

---
+++ app/src/test/java/com/gonzalo/racetracker/RaceParticipantTest.kt
@@ -0,0 +1,40 @@
+package com.gonzalo.racetracker
+
+import com.gonzalo.racetracker.ui.RaceParticipant
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.advanceTimeBy
+import kotlinx.coroutines.test.runCurrent
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+
+class RaceParticipantTest {
+    private val raceParticipant = RaceParticipant(
+        name = "Test",
+        maxProgress = 100,
+        progressDelayMillis = 500L,
+        initialProgress = 0,
+        progressIncrement = 1
+    )
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun raceParticipant_RaceStarted_ProgressUpdated() = runTest {
+        val expectedProgress = 1
+
+        launch { raceParticipant.run() }
+        advanceTimeBy(raceParticipant.progressDelayMillis)
+        runCurrent()
+        assertEquals(expectedProgress, raceParticipant.currentProgress)
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun raceParticipant_RaceFinished_ProgressUpdated() = runTest {
+        launch { raceParticipant.run() }
+        advanceTimeBy(raceParticipant.maxProgress * raceParticipant.progressDelayMillis)
+        runCurrent()
+        assertEquals(raceParticipant.maxProgress, raceParticipant.currentProgress)
+    }
+}
